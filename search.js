66be6824a27d028fd71af564fb718153b5d88ddb
EKaSusanto12 <86885759+EKaSusanto12@users.noreply.github.com>
2023-06-18 13:30:48 +0700

    Add files via upload

---
+++ search.js
@@ -0,0 +1,22 @@
+function Function() {
+  var input, filter, ul, li, a, i;
+  input = document.getElementById("Search");
+  filter = input.value.toUpperCase();
+  ul = document.getElementById("Menu");
+  li = ul.getElementsByTagName("li");
+
+  for (i = 0; i < li.length; i++) {
+    a = li[i].getElementsByTagName("a")[0];
+    if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "";
+    } else {
+      li[i].style.display = "none";
+    }
+  }
+};
+
+function enter() {
+    if (event.keyCode === 13 || event.key === "Enter") {
+        Function()
+    }
+}
