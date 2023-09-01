a98dd86ec9c90abfe3284adf5a21f348b4b1aa53
EKaSusanto12 <86885759+EKaSusanto12@users.noreply.github.com>
2023-09-01 19:07:42 +0800

    update main.js

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
-var audio= document.getElementsByTagName("audio");
-var navActive= document.getElementsByClassName("active");
-if (navActive) {
- audio.style.display = "none";
- navActive = true:
-} else {
- audio.style.display = "block";
- navActive = false;
+var audio = document.getElementsByTagName("audio")[0]; // Mengambil elemen pertama dengan tag "audio"
+var navActive = document.getElementsByClassName("active");
+
+if (audio && navActive.length > 0) { // Memeriksa apakah elemen audio dan elemen dengan kelas "active" ada
+  audio.style.display = "none"; // Menyembunyikan elemen audio
+} else if (audio) {
+  audio.style.display = "block"; // Menampilkan elemen audio jika elemen dengan kelas "active" tidak ada
 }
