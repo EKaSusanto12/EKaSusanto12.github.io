ed25b276ec73f1fd564d6c5b26c25a3d94506035
EKaSusanto12 <86885759+EKaSusanto12@users.noreply.github.com>
2023-09-01 18:55:18 +0800

    update main.js

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ var audio= document.getElementsByTagName("audio");
 var navActive= document.getElementsByClassName("active");
 if (navActive) {
  audio.style.display = "none";
+ navActive = true:
 } else {
  audio.style.display = "block";
+ navActive = false;
 }
